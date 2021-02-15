c82bcb94a0a13db68ea2bee1f706a25a60a3d84a
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2021-02-15 16:27:54 +0100

    primer commit

---
+++ src/main/java/Lab42Trim/isabel/IsabelApplication.java
@@ -0,0 +1,13 @@
+package Lab42Trim.isabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lab42Trim/isabel/IsabelApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab42Trim.isabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
